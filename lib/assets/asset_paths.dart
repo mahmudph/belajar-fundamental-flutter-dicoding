cc5325ec5491922d56edd3c1defa53e0be5b5809
mahmud <mahmud120398@gmail.com>
2022-11-20 12:31:31 +0700

    feat: add new asset

--- lib/assets/asset_paths.dart
+++ lib/assets/asset_paths.dart
@@ -6,8 +6,12 @@
  */
 
 class AssetPaths {
-  static const String appLogo = 'lib/assets/images/icon.png';
-  static const String error = 'lib/assets/images/error.png';
-  static const String notFound = 'lib/assets/images/not_found.png';
-  static const String restaurantData = "lib/assets/data/restaurant.json";
+  static const String assetPath = "lib/assets";
+  static const String appLogo = '$assetPath/images/icon.png';
+  static const String search = '$assetPath/images/search.png';
+  static const String error = '$assetPath/images/error.png';
+  static const String notFound = '$assetPath/images/not_found.png';
+  static const String restaurantData = "$assetPath/data/restaurant.json";
+  static const String placeholderImage =
+      "$assetPath/images/placeholder-image.png";
 }
