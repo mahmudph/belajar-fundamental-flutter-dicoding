d431f09b7f8561e28129c55ed052c43931c5b8a2
mahmud <mahmud120398@gmail.com>
2022-11-23 08:40:49 +0700

    refactor: update mock class

--- test/dependency_bloc_test/dependency_setting_bloc_test.dart
+++ test/dependency_bloc_test/dependency_setting_bloc_test.dart
@@ -9,22 +9,22 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mahmud_flutter_restauran/ui/screens/settings/cubit/settings_cubit.dart';
 import 'package:mahmud_flutter_restauran/utils/scheduler.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class MockSharePreference extends Mock implements SharedPreferences {}
+import 'dependency_setting_bloc_test.mocks.dart';
 
-class MockScheduler extends Mock implements Scheduler {}
-
+@GenerateMocks([SharedPreferences, Scheduler])
 void main() {
   group("test settings bloc", () {
     late MockScheduler mockScheduler;
-    late MockSharePreference mockSharePreference;
+    late MockSharedPreferences mockSharePreference;
     late SettingsCubit settingsCubit;
 
     setUp(() {
       mockScheduler = MockScheduler();
-      mockSharePreference = MockSharePreference();
+      mockSharePreference = MockSharedPreferences();
       settingsCubit = SettingsCubit(
         scheduler: mockScheduler,
         sharedPreferences: Future.value(mockSharePreference),
@@ -50,7 +50,7 @@ void main() {
       "emit state [SettingsSuccess] when load page",
       setUp: () {
         when(mockSharePreference.setBool('IS_ENABLE_SCHEDULE', true))
-            .thenReturn(Future.value(true));
+            .thenAnswer((_) async => true);
 
         when(mockScheduler.startPeriodictNotification())
             .thenAnswer((_) async => {});
@@ -58,11 +58,11 @@ void main() {
       build: () => settingsCubit,
       act: (bloc) => bloc.updateScheduleNotifConfig(true),
       expect: () => [
-        const SettingsSuccess(isEnableScheduleNotif: false),
+        const SettingsSuccess(isEnableScheduleNotif: true),
       ],
       verify: (bloc) {
         expect(bloc.state, isA<SettingsSuccess>());
-        expect((bloc.state as SettingsSuccess).isEnableScheduleNotif, false);
+        expect((bloc.state as SettingsSuccess).isEnableScheduleNotif, true);
         verify(mockScheduler.startPeriodictNotification()).called(1);
         verify(mockSharePreference.setBool("IS_ENABLE_SCHEDULE", true))
             .called(1);
